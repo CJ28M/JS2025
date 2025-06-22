5115dd0aa41d720434cd232f18300f6c23084fa1
CJ28M <marin.christopher128@gmail.com>
2025-06-21 20:01:21 -0700

    index, data page

---
+++ index.js
@@ -0,0 +1,15 @@
+var name = "CJ";
+var lastName = "Marin";
+var fullName = name + " " + lastName;
+console.log(fullName);
+
+var userName = "CJ";
+var totalWins = 30;
+var totalLosses = 10;
+var userTotalLosses = totalLosses + 5;
+var userInfo =
+  userName + " has " + userTotalLosses + " losses and " + totalWins + " wins.";
+console.log(userInfo);
+
+/* if you want to comment out a block of code, you can use alt+shift+a */
+// if you want to comment out a inline code, you can use ctrl+/
