cd1060f9d25b4432ee35dd740a82a2d38e90e3ba
CJ28M <marin.christopher128@gmail.com>
2025-06-23 14:22:03 -0700

    numbers page added

--- js/string.js
+++ js/string.js
@@ -20,3 +20,45 @@ var myRide = make + " " + model;
 var myBraggingRight = "I drive a " + year + " " + myRide;
 
 console.log(myRide.length);
+
+//indexOf()
+
+let myString = "Hello World";
+let index = myString.indexOf("o"); // Finds the first 'o'
+console.log(index); // Output: 4 (index of the first 'o')
+
+let index2 = myString.indexOf("World"); // Finds the substring "World"
+console.log(index2); // Output: 6 (index of the start of "World")
+
+//with arrays:
+
+const groceryList = [
+  "Milk",
+  "Butter",
+  "Eggs",
+  "Chicken",
+  "Steak",
+  "Rice",
+  "Corn",
+  "Eggs",
+];
+
+console.log(groceryList.indexOf("Eggs"));
+console.log(groceryList.indexOf("Carrots"));
+console.log(groceryList.indexOf("Eggs", 3));
+
+let text = "Hello planet earth, you are a great planet.";
+let result = text.lastIndexOf("planet");
+
+console.log(result);
+
+let textInfo = "Mr Blue has a blue house and a blue car";
+let results = textInfo.replace("blue", "red");
+
+console.log(results);
+
+let resultss = textInfo.replace("blue", (match) => {
+  return match.toUpperCase();
+});
+
+console.log(resultss);
