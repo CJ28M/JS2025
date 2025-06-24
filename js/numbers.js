c6cd8550eec1f6ce885c44c8d28379f059b0314e
CJ28M <marin.christopher128@gmail.com>
2025-06-23 21:43:28 -0700

    unsolved toFixed()

---
+++ js/numbers.js
@@ -0,0 +1,39 @@
+var totalUsers = 42;
+var purchase = 46.99;
+
+var sciNot = 123e5;
+var sciNott = 123e-5;
+
+var numVal = 5 + 5;
+var numMin = 5 - 5;
+var numMul = 5 * 5;
+var numDiv = 5 / 5;
+
+var numRem = 12 % 5;
+var numRemm = 19 % 5;
+
+console.log(numRemm);
+
+var totalUserNum = 999999999999999;
+var moreThenFif = 99999999999999999;
+
+console.log(moreThenFif);
+
+var randomNum = 123456789101112131415161718;
+
+console.log(randomNum);
+
+var numDec = 0.1 + 0.2;
+
+console.log(numDec);
+
+var numOps = (0.1 * 10 + 0.2 * 10) / 10;
+
+console.log(numOps);
+
+var numStr = 10 + "5 years";
+
+console.log(numStr);
+
+var numStri = 10 + "5";
+console.log(numStri);
