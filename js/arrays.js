00aaecf0bc4709d8ba146fad15a5209b32d9689f
CJ28M <marin.christopher128@gmail.com>
2025-06-25 17:42:18 -0700

    loops page

---
+++ js/arrays.js
@@ -0,0 +1,13 @@
+var tvshows = ["gold rush", "breaking bad", "the big bang theory"];
+
+var userSavedMovies = [];
+
+userSavedMovies.push("Minecraft");
+userSavedMovies.push("Scary movie");
+userSavedMovies.push("Room 1908");
+
+userSavedMovies.pop();
+
+console.log(userSavedMovies);
+
+console.log(tvshows);
