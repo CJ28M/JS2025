f992c8d57d3d8c32f0c2031563aae85a5637b191
CJ28M <marin.christopher128@gmail.com>
2025-08-26 21:11:28 -0400

    using the switch statement page 60

--- javascriptQSG/scratch.js
+++ javascriptQSG/scratch.js
@@ -33,6 +33,47 @@ let shoppingList = ["Milk", "Eggs", "Cheese", "Bread"];
 console.log("the second item on the shopping list is " + shoppingList[1]);
 
 console.log(
-  "we can create a string from the items of an array with join" +
+  "we can create a string from the items of an array with join " +
     shoppingList.join(", ")
 );
+
+shoppingList.push("Oranges");
+
+console.log(
+  "I forgot the oranges, lets add it by using .push: " + shoppingList.join(", ")
+);
+
+shoppingList[0] = "Almond Milk";
+console.log("updated shopping list: " + shoppingList.join(", "));
+
+//page 45
+
+//---------------------------Logical Comparisons---------------------------------------
+
+let price = 4.95;
+if (price == 4.95) {
+  console.log("The Price is 4.95");
+} else {
+  console.log("The price is something else.");
+}
+
+//------------------------SWITCH STATEMENT------------------------------------
+// acts like a switch board
+
+let color = "blue";
+
+switch (color) {
+  case "red":
+    console.log("The color is red.");
+    break;
+  case "orange":
+    console.log("The color is orange.");
+    break;
+  case "purple":
+    console.log("The color is purple.");
+    break;
+  default:
+    console.log("I'm not sure what color it is.");
+}
+
+//page: 58
