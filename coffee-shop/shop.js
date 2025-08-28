d0d9862224cd310d8c7ea26716efd08fed0e7ae7
CJ28M <marin.christopher128@gmail.com>
2025-08-27 20:03:00 -0400

    starting chapter 4

--- coffee-shop/shop.js
+++ coffee-shop/shop.js
@@ -1,2 +1,15 @@
 // For now, display a quick message in the console.
 console.log("ClydeBank Coffee Shop is now open!");
+
+//Our Inventory
+let inventory = ["Regular Coffee", "Espresso", "Cappuccino", "Latte"];
+let inventoryPrices = [3.0, 3.5, 4.0, 4.25];
+
+//Obtain reference to the menu list by ID
+let menuList = document.getElementById("coffee-menu");
+
+//loop through the inventory and display each item in the menu list
+for (let i = 0; i < inventory.length; i++) {
+  menuList.innerHTML +=
+    "<li>" + inventory[i] + " - $" + inventoryPrices[i].toFixed(2) + "</li>";
+}
